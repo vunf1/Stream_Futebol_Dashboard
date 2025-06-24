96d6ef4a32ba9ea8455ccfc98fdc5362de82c498
Vunf1 <jokass.workplace@gmail.com>
2025-06-24 17:03:47 +0100

    Add build script and requirements for customtkinter and ctkmessagebox

--- goal/goal-static/build.py
+++ goal/goal-static/build.py
@@ -1,13 +1,20 @@
 import subprocess
 
+# Install dependencies
+subprocess.run(["python", "-m", "pip", "install", "-r", "requirements.txt"])
+
 pyinstaller_command = [
-    "pyinstaller",
+    "python", "-m", "PyInstaller",
     "--clean",
     "--onefile",
     "--noconsole",
+    "--hidden-import", "customtkinter",
+    "--hidden-import", "ctkmessagebox",
     "--icon", "assets/icons/icon_soft.ico",
+    "--version-file", "version.txt", 
     "goal_score.py"
 ]
+
 try:
     subprocess.run(pyinstaller_command, check=True)
     print("Build completed successfully!")
